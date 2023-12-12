02196d5d6331a29277add804be66d236db6443e4
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-12-12 09:37:49 -0500

    mon

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,9 @@
 import "../styles/style.css"
-const URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata/images/media/meals/llcbn01574260722.jpg/preview";
+const URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
 
 async function test(){
   let res = await  fetch(URL)
   let data = await res.json()
-  console.log(data)
+  console.log(data); 
 }
 test()                                         
