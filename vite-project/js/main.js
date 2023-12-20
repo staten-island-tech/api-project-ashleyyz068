a4b260da20724ab90d31c344768a8476d59f38ff
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-12-20 09:20:16 -0500

    gh

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,11 +1,11 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./Dom";
-const URL = " https://www.themealdb.com/api/json/v1/1/search.php?f=a";
-async function test(){
+const URL = " https://www.themealdb.com/api/json/v1/1/search.php?f=";
     let res = await  fetch(URL)
     let data = await res.json()
-    
-  }
+  console.log(data.meals[1].idMeal);
+
+  
                                      
   
                                        
